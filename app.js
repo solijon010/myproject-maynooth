4101e1d00c7a358bca97e17e306d66bee3b156bb
Sal1kh.Ake <solijonikromov001@gmail.com>
2025-08-15 13:36:25 +0500

    html ga ozgartirish kiritdim

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const overlay = document.querySelector(".extra__sitenav-overlay");
 const sitenav = document.querySelector(".extra-sitenav");
 const closeBtn = document.querySelector(".extra__sitenav-btn");
 
+const filterToggle = document.getElementById('filterToggle');
+const filterPanel = document.getElementById('filterPanel');
+const filterClose = document.getElementById('filterClose');
+
+
+
 showSitenavBtn.addEventListener("click", function () {
   overlay.style.transform = "translate(0)";
   sitenav.style.transform = "translateX(0)";
@@ -15,4 +21,12 @@ overlay.addEventListener("click", function (e) {
 closeBtn.addEventListener("click", function (e) {
   overlay.style.transform = "translate(100%)";
   sitenav.style.transform = "translateX(100%)";
+});
+
+filterToggle.addEventListener('click', () => {
+    filterPanel.style.display = 'block';
+});
+
+filterClose.addEventListener('click', () => {
+    filterPanel.style.display = 'none';
 });
